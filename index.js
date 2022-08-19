7e6334e817a9ab8ad21d0bd5a80b0c1793ae141b
Cesar Hernandez <chema27281@gmail.com>
2022-08-18 20:33:25 -0400

    commit tree 804ebab73d23d05ef307172729c2e47d119a94a1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const cards= (["Venam","Eris","Melia"], "surprise");
+function writeCards(cards,surprise){
+  let presents=[]
+for (let i = 0; i < cards.length; i++){
+   presents.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+}
+return presents
+}
+function countDown(){
+  let number = 10
+  while (number >= 0){
+    console.log(number--)
+  }
+}
